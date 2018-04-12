5a423689d027884f8d38ce5dbdbf6f03f0340dc4
superpuper32 <pochitalin_m@mail.ru>
2018-04-12 20:11:58 +0300

    Created Variables and Fuctions

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,28 @@
 // All code should be written in this file.
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
+
+function setPlayerMoves() {
+
+}
+
+function getRoundWinner() {
+
+}
+
+function getGameWinner() {
+
+}
+
+function setComputerMoves() {
+  
+}
